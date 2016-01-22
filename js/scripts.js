68e6992b1177c3891aad057e4bb058752b57eb12
Dustin <hiitsdustindavis@gmail.com>
2016-01-22 09:51:34 -0800

    Setup index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,37 @@
-var nonnumber = function(nonnumber) {
-  if (nonnumber <= 0); {
-    return false;
-  }
-};
+//BUSINESS LOGIC
 var number = function(number) {
- if (number % 3 === 0); {
+if (number <= 0) {
+  return false;
+} else if (number % 3 === 0){
   return "ping";
-
-} if (number % 5 === 0); {
+} else if (number % 5 === 0) {
   return "pong";
+} else {
+  return "NaN";
 }
 };
 
-//   return false;
-// };
-// if (number % 3 === 0) {
-// return "ping";
-// } else {
-  // (number <= 0);
+// USER INTERFACE LOGIC
+$(function() {
+  var number = function(loopEnd) {
+    alert("hey");
+  var output = [];
+  for(var i = 0; i <= loopEnd; i ++) {
+      output.push(i);
+}
+  return output;
+};
+});
+  $("form#counter").submit(function(event) {
+
+      var loopEnd =
+      parseInt($("input#count-to").val());
+      debugger;
+      var numbers = counter(loopEnd, multiple);
+
+      $(".output").text(numbers);
+
+      $("#result").show();
+      event.preventDefault();
+
+    });
--- spec/specs.js
+++ spec/specs.js
@@ -1,16 +1,14 @@
-describe('nonnumber', function() {
-  it("will return false if user enters a number less than one", function() {
-    expect(nonnumber(0)).to.equal(false);
+describe('number', function() {
+  it("is false if user enters a number less than one", function() {
+    expect(number(0)).to.equal(false);
   });
   it("is false if user enters a non number", function() {
-    expect(nonnumber("brad")).to.equal(false);
+    expect(number("brad")).to.equal("NaN");
   });
-});
-describe('number', function() {
-  it("return 'ping' if number is divisible by 3", function() {
-    expect(number("6")).to.equal("ping");
+  it("returns 'ping' if number is divisible by 3", function() {
+    expect(number(3)).to.equal("ping");
   });
   it("return 'pong' if number is divisible by 5", function() {
-    expect(number("10")).to.equal("pong");
+    expect(number(10)).to.equal("pong");
   });
 });
