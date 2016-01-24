2716eb5d196499066927e1afcbddb1c94e2583cf
Dustin <hiitsdustindavis@gmail.com>
2016-01-23 23:23:38 -0800

    hot fix indentation

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,6 @@ var output = [];
 
 // USER INTERFACE LOGIC
 $(function() {
-
   $("form#counter").submit(function(event) {
     // debugger;
     var gnome = $("input#count-to").val();
@@ -31,80 +30,7 @@ $(function() {
       for ( i = 0; i < result.length; i ++) {
         list += "<li>" + result[i] + "</li>";
       } document.getElementById("output").innerHTML = list;
-        // document.write(result[i]);
-      // }
-    // result.toString("#output");
+
     event.preventDefault();
   });
 });
-
-
-  // $(function() {
-//   var counter = function(loopEnd) {
-//   var output = [];
-//
-//     for (i = 0; i <= loopEnd; i ++) {
-//     } if (i % 3 === 0) {
-//       output.push("ping")
-//     } else if (i % 5 === 0) {
-//       output.push("pong")
-//     } else {
-//       output.push(i);
-//     }
-//   };
-
-//   $("form#counter").submit(function(event) {
-//     var numbers = [counter(loopEnd)];
-//     var loopEnd = $("input#count-to").val();
-//
-//
-//
-//       // for (var i = 0; i < number.length; i ++) {
-//       //   listItem += "<li>" + result[i] + "</li>";
-//       // }
-//
-//
-//     $("#output").text(numbers);
-//
-//     $("#result").show();
-//     event.preventDefault();
-//
-//   });
-// });
-
-// else if (i % 3 !==0 || i % 5 !== 0){
-//   output.push(i)
-// }
-// document.getElementById("output").innerHTML = "Good day";
-
-// PRE CONDITIONAL STATEMENT WORK
-// $(function() {
-//   var counter = function(loopEnd) {
-//     var output = [];
-//     for(var i = 1; i <= loopEnd; i ++) {
-//         output.push(i);
-//     }
-//     return output;
-//   };
-
-// $(function() {
-//   var pingPong = function(number) {
-//   var output = [];
-//   for(var i = 1; i <= number; i ++) {
-//       output.push(i);
-// }
-//   return output;
-// };
-// });
-//   $("form#counter").submit(function(event) {
-//
-//       var numberEnd =
-//       parseInt($("input#user-number").val());
-//       var result = pinPong(numberEnd);
-//
-//       $(".output").text(result);
-//
-//       $("#result").show();
-//       event.preventDefault();
-//
-//     });
